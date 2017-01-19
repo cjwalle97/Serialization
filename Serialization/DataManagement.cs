c1822377d7955b10ba53cb675932ed7f83dbd4a8
cjwalle97 <cjwalle97@gmail.com>
2017-01-19 11:21:17 -0600

    Initial commit
    Initial commit

---
+++ Serialization/DataManagement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializationTutorial
+{
+
+    static class DataManagement<T>
+    {
+        public static void serialization(string fileName, T data)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            TextWriter writter = new StreamWriter(@"..\..\SavedFiles\" + fileName + ".xml");
+            serializer.Serialize(writter, data);
+            writter.Close();
+        }
+        public static T Deserialize(string fileName)
+        {
+            T data;
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            TextReader reader = new StreamReader(@"..\..\SavedFile\" + fileName + ".xml");
+            data = (T)serializer.Deserialize(reader);
+            return data;
+        }
+    }
+}
---
+++ Serialization/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationTutorial
+{
+    class Student
+    {
+        private string mName;
+        private int mAge;
+        private string mID;
+
+        public Student() { }
+        public Student(string n, int a, string i)
+        {
+            mName = n;
+            mAge = a;
+            mID = i;
+        }
+        public string NAME
+        {
+            get
+            {
+                return mName;
+            }
+            set
+            {
+                mName = null;
+            }
+        }
+
+        public int AGE
+        {
+            get
+            {
+                return mAge;
+            }
+            set
+            {
+                mAge = 0;
+            }
+        }
+        public string ID;
+    }
+}
