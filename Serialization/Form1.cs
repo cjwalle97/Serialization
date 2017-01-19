ed2df7086a2a78eb7343ed8c1ddd88d8cd477cdb
cjwalle97 <cjwalle97@gmail.com>
2017-01-19 11:57:21 -0600

    Update
    Update

--- Serialization/Form1.cs
+++ Serialization/Form1.cs
@@ -16,5 +16,15 @@ namespace Serialization
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Serialization/Student.cs
+++ Serialization/Student.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SerializationTutorial
+
+namespace Serialization
 {
     class Student
     {
